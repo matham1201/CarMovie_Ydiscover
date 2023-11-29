0b3e0c17e80290a36f2472e5a2e8bf8ed09d7ecf
matheo.hamon@ynov.com <matheo.hamon@ynov.com>
2023-11-29 14:39:20 +0100

    Create tables

--- public/migrations/create_movie.sql
+++ public/migrations/create_movie.sql
@@ -3,8 +3,9 @@ CREATE TABLE IF NOT EXISTS movie (
     title TEXT NOT NULL,
     year INTEGER NOT NULL,
     genre TEXT NOT NULL,
-    rating INTEGER NOT NULL,
-    description TEXT NOT NULL,
-    image TEXT NOT NULL
+    rating INTEGER NOT NULL
+    id_car INTEGER NOT NULL,
+    FOREIGN KEY (id_car) REFERENCES car(id)
 );
+
 ``` 
