d2527aa1f15f10e37a1b125fd54b5c124b07fe49
Harcadakad <evan.guillet@ynov.com>
2023-11-29 14:28:57 +0100

    :sparkles: - Add timer of questions

---
+++ scripts/quiz.js
@@ -0,0 +1,24 @@
+let seconds = document.getElementById('seconds');
+let ss = document.getElementById('ss');
+let sec_dot = document.querySelector('.sec_dot');
+
+let timer = 19;
+
+const interval = 1000;
+
+const time = setInterval(updateTimer, interval);
+
+ss.style.strokeDashoffset = 1;
+
+function updateTimer() {
+    if(timer != 0){
+        timer--;
+    }
+
+    seconds.innerHTML = timer;
+    ss.style.strokeDashoffset = -(440 - (440 * timer)/19);
+    sec_dot.style.transform = `rotateZ(${-(timer*19)}deg)`;
+}
+
+//animation
+
